18c32c4df9a30a77c876b49c4a94360c34a5094c
Halimah Akinoso <halimahAkin2@gmail.com>
2021-07-14 03:04:43 +0100

    netbean 8 maven ist commit

---
+++ src/main/java/com/mycompany/mavenprjt2/App.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenprjt2;
+
+/**
+ *
+ * @author SOLACE
+ */
+public class App {
+    
+}
